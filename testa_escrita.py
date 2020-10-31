1d68f5b5f1a621ead4c78665257585e08ae91692
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-30 23:42:31 -0300

    'close_flush'

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,5 +1,9 @@
 arquivo_contatos = open('dados/contatos-escrita.csv', encoding='latin_1', mode='w')
 
-contato = '11,Henrique,henrique@henrique.com\n'
+contatos = ['11,Henrique,henrique@henrique.com\n'
+           '12,Guilherme,guilherme@guilherme.com\n']
 
-arquivo_contatos.write(contato)
+for i in contatos:
+    arquivo_contatos.write(i)
+
+arquivo_contatos.flush()
