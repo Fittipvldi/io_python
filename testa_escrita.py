f37101f9152f63ca12d2569b9371fc018aa4f1c5
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-11-01 21:53:39 -0300

    escrita_leitura

--- testa_escrita.py
+++ testa_escrita.py
@@ -1,4 +1,4 @@
-arquivo_contatos = open('dados/contatos-escrita.csv', encoding='latin_1', mode='w')
+arquivo_contatos = open('dados/contatos-escrita.csv', encoding='latin_1', mode='w+')
 
 contatos = ['11,Henrique,henrique@henrique.com\n'
            '12,Guilherme,guilherme@guilherme.com\n']
@@ -7,3 +7,11 @@ for i in contatos:
     arquivo_contatos.write(i)
 
 arquivo_contatos.flush()
+
+arquivo_contatos.seek(0)
+arquivo_contatos.write('11,Henrique,henrique@henrique.com\n'.upper())
+arquivo_contatos.flush()
+arquivo_contatos.seek(0)
+
+for i in arquivo_contatos:
+    print(i)
