e4e561fe5ad7ad4c8a2256b9993e0d250be7b5af
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-11-01 22:58:54 -0300

    escrita_buffer

--- testa_io.py
+++ testa_io.py
@@ -1,11 +1,14 @@
-arquivo = open('dados/contatos.csv', encoding='latin_1')
+arquivo = open('dados/contatos-escrita.csv', encoding='latin_1', mode='a+')
 
 print(type(arquivo.buffer))
 
 conteudo = arquivo.buffer.read()
 
 texto_em_bytes = bytes('Esse é um texto em bytes', 'latin_1')
-print(texto_em_bytes)
-print(type(texto_em_bytes))
+# print(texto_em_bytes)
+# print(type(texto_em_bytes))
+
+contato = bytes('15,Flavio,flavio@flavio.com.br\n', 'latin_1')
+arquivo.buffer.write(contato)
 
 arquivo.close()
