2e0ee0565708037f9a596b358ab242cf039b8d70
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-11-02 23:28:18 -0300

    python_para_pickle

--- contatos_uteis.py
+++ contatos_uteis.py
@@ -1,4 +1,4 @@
-import csv
+import csv, pickle
 from contato import Contato
 
 
@@ -17,4 +17,16 @@ def csv_para_contatos(caminho, encoding):
 
             contato = Contato(id, nome, email)
             contatos.append(contato)
+    return contatos
+
+
+def contatos_para_pickle(contatos, caminho):
+    with open(caminho, mode='wb') as file:
+        pickle.dump(contatos, file)
+
+
+def pickle_para_contatos(caminho):
+    with open(caminho, mode='rb') as file:
+        contatos = pickle.load(file)
+
     return contatos
--- principal.py
+++ principal.py
@@ -2,7 +2,10 @@ import contatos_uteis
 
 
 try:
-    contatos = contatos_uteis.csv_para_contatos('dados/contatos.csv', 'latin_1')
+    # contatos = contatos_uteis.csv_para_contatos('dados/contatos.csv', 'latin_1')
+    # contatos_uteis.contatos_para_pickle(contatos, 'dados/contatos.p')
+
+    contatos = contatos_uteis.pickle_para_contatos('dados/contatos.p')
 
     for i in contatos:
         print(f'{i.id} - {i.nome} - {i.email}')
